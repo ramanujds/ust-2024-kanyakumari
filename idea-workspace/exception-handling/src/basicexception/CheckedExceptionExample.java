b8fda429ac4dfc7e722b5e803c6c608bb24d085b
Ramanuj Das <ramanujds9@gmail.com>
2024-08-26 17:26:47 +0530

    Updated

--- idea-workspace/exception-handling/src/basicexception/CheckedExceptionExample.java
+++ idea-workspace/exception-handling/src/basicexception/CheckedExceptionExample.java
@@ -2,14 +2,27 @@ package basicexception;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class CheckedExceptionExample {
 
-    public static void main(String[] args) throws FileNotFoundException,RuntimeException {
+    public static void main(String[] args)  {
 
 
-        FileReader reader = new FileReader("hello.txt");
+        FileReader reader = null;
+        try {
+            reader = new FileReader("hello.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         Scanner scanner = new Scanner(reader);
         System.out.println(scanner.nextLine());
 
