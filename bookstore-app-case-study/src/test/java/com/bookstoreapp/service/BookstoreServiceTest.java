28c9edac7b59daea4795b1c05aa52d84abeaa2c0
Ramanuj Das <ramanujds9@gmail.com>
2024-09-18 14:59:57 +0530

    updated test cases

--- bookstore-app-case-study/src/test/java/com/bookstoreapp/service/BookstoreServiceTest.java
+++ bookstore-app-case-study/src/test/java/com/bookstoreapp/service/BookstoreServiceTest.java
@@ -29,8 +29,6 @@ public class BookstoreServiceTest {
     private BookRepository bookRepository;
 
 
-
-
     // write test for getAllBooks()
     @Test
     public void testGetAllBooks() {
@@ -38,10 +36,10 @@ public class BookstoreServiceTest {
         // Given
         // create a list of books
         List<Book> books = new ArrayList<>();
-        books.add(new Book(1, "The Alchemist", "Paulo Coelho", 1988,156266262611L,250.0));
-        books.add(new Book(2, "The Great Gatsby", "F. Scott Fitzgerald", 1925,156266262612L,300.0));
-        books.add(new Book(3, "The Kite Runner", "Khaled Hosseini", 2003,156266262613L,350.0));
-        books.add(new Book(4, "The Little Prince", "Antoine de Saint-Exupery", 1943,156266262614L,400.0));
+        books.add(new Book(1, "The Alchemist", "Paulo Coelho", 1988, 156266262611L, 250.0));
+        books.add(new Book(2, "The Great Gatsby", "F. Scott Fitzgerald", 1925, 156266262612L, 300.0));
+        books.add(new Book(3, "The Kite Runner", "Khaled Hosseini", 2003, 156266262613L, 350.0));
+        books.add(new Book(4, "The Little Prince", "Antoine de Saint-Exupery", 1943, 156266262614L, 400.0));
 
         // When
         // mock the repository
@@ -60,139 +58,144 @@ public class BookstoreServiceTest {
 
     // write test for getBookById()
     @Test
-    public void testGetBookById(){
+    public void testGetBookById() {
 
-            // Given
-            // create a book
-            Book book = new Book(1, "The Alchemist", "Paulo Coelho", 1988,156266262611L,250.0);
+        // Given
+        // create a book
+        Book book = new Book(1, "The Alchemist", "Paulo Coelho", 1988, 156266262611L, 250.0);
 
-            // When
-            // mock the repository
+        // When
+        // mock the repository
 
-            when(bookRepository.findById(1)).thenReturn(Optional.of(book));
+        when(bookRepository.findById(1)).thenReturn(Optional.of(book));
 
-            // Then
-            // check if the result is equal to the expected result
-            assertEquals(book, bookstoreService.getBookById(1));
+        // Then
+        // check if the result is equal to the expected result
+        assertEquals(book, bookstoreService.getBookById(1));
 
-            // check if the method is called only once
-            verify(bookRepository, times(1)).findById(1);
+        // check if the method is called only once
+        verify(bookRepository, times(1)).findById(1);
 
     }
 
 
     // write test for saveBook()
     @Test
-    public void testSaveBook(){
+    public void testSaveBook() {
 
-            // Given
-            // create a book
-            Book book = new Book(1, "The Alchemist", "Paulo Coelho", 1988,156266262611L,250.0);
+        // Given
+        // create a book
+        Book book = new Book(1, "The Alchemist", "Paulo Coelho", 1988, 156266262611L, 250.0);
 
-            // When
-            // mock the repository
-            when(bookRepository.save(book)).thenReturn(book);
+        // When
+        // mock the repository
+        when(bookRepository.findById(1)).thenReturn(Optional.empty());
+        when(bookRepository.save(book)).thenReturn(book);
 
-            // Then
-            // check if the result is equal to the expected result
-            assertEquals(book, bookstoreService.saveBook(book));
+        // Then
+        // check if the result is equal to the expected result
+        assertEquals(book, bookstoreService.saveBook(book));
 
-            // check if the method is called only once
-            verify(bookRepository, times(1)).save(book);
+        // check if the method is called only once
+        verify(bookRepository, times(1)).save(book);
 
     }
 
     // write test for updateBook()
     @Test
-    public void testUpdateBook(){
+    public void testUpdateBook() {
 
-            // Given
-            // create a book
-            Book book = new Book(1, "The Alchemist", "Paulo Coelho", 1988,156266262611L,250.0);
+        // Given
+        // create a book
+        Book book = new Book(1, "The Alchemist", "Paulo Coelho", 1988, 156266262611L, 250.0);
 
-            // When
-            // mock the repository
-            when(bookRepository.update(book)).thenReturn(book);
+        // When
+        // mock the repository
+        when(bookRepository.findById(1)).thenReturn(Optional.of(book));
+        when(bookRepository.update(book)).thenReturn(book);
 
 
-            // Then
-            // check if the result is equal to the expected result
-            assertEquals(book, bookstoreService.updateBook(1, book));
+        // Then
+        // check if the result is equal to the expected result
+        assertEquals(book, bookstoreService.updateBook(1, book));
 
-            // check if the method is called only once
-            verify(bookRepository, times(1)).update(book);
+        // check if the method is called only once
+        verify(bookRepository, times(1)).update(book);
 
     }
 
     // write test for deleteBook()
     @Test
-    public void testDeleteBook(){
+    public void testDeleteBook() {
+
+        // Given
+
+        Book book = new Book(1, "The Alchemist", "Paulo Coelho", 1988, 156266262611L, 250.0);
 
 
-            // When
-            // mock the repository
-            doNothing().when(bookRepository).deleteById(1);
+        // When
+        // mock the repository
+        doNothing().when(bookRepository).deleteById(1);
+        when(bookRepository.findById(1)).thenReturn(Optional.of(book));
 
-            // Then
-            // check if the result is equal to the expected result
+        // Then
+        // check if the result is equal to the expected result
         assertDoesNotThrow(() -> {
             bookstoreService.deleteBook(1);
         });
 
-            // check if the method is called only once
-            verify(bookRepository, times(1)).deleteById(1);
+        // check if the method is called only once
+        verify(bookRepository, times(1)).deleteById(1);
 
     }
 
     // Write test for getBookById() when book is not found
     @Test
-    public void testGetBookByIdWhenBookNotFound(){
+    public void testGetBookByIdWhenBookNotFound() {
 
-            // Given
+        // Given
 
 
-            // When
-            // mock the repository
-            when(bookRepository.findById(1)).thenThrow(new BookNotFoundException("Book with ID: 1 Not Found"));
+        // When
+        // mock the repository
+        when(bookRepository.findById(1)).thenReturn(Optional.empty());
 
-            // Then
-            // check if BookNotFoundException is thrown
+        // Then
+        // check if BookNotFoundException is thrown
 
-            assertThrows(BookNotFoundException.class, () -> {
+        assertThrows(BookNotFoundException.class, () -> {
             bookstoreService.getBookById(1);
-            });
+        });
 
-            // check if the method is called only once
-            verify(bookRepository, times(1)).findById(1);
+        // check if the method is called only once
+        verify(bookRepository, times(1)).findById(1);
 
     }
 
     // Write test for saveBook when book is already exists
 
     @Test
-    public void testSaveBookWhenBookAlreadyExists(){
+    public void testSaveBookWhenBookAlreadyExists() {
 
-            // Given
-            // create a book
-            Book book = new Book(1, "The Alchemist", "Paulo Coelho", 1988,156266262611L,250.0);
+        // Given
+        // create a book
+        Book book = new Book(1, "The Alchemist", "Paulo Coelho", 1988, 156266262611L, 250.0);
 
-            // When
-            // mock the repository
+        // When
+        // mock the repository
 
-            when(bookRepository.save(book)).thenThrow(new BookAlreadyExistsException("Book already exists"));
+        when(bookRepository.save(book)).thenThrow(new BookAlreadyExistsException("Book already exists"));
 
-            // Then
-            // check if BookAlreadyExistsException is thrown
-            assertThrows(BookAlreadyExistsException.class, () -> {
+        // Then
+        // check if BookAlreadyExistsException is thrown
+        assertThrows(BookAlreadyExistsException.class, () -> {
             bookstoreService.saveBook(book);
-            });
+        });
 
-            // check if the method is called only once
-            verify(bookRepository, times(1)).save(book);
+        // check if the method is called only once
+        verify(bookRepository, times(1)).save(book);
 
     }
-
-
 
 
 }
