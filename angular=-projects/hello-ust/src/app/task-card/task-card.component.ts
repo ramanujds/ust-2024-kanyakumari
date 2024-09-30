fa2a062671006a7ab8b8af69cf27612f54767018
Ramanuj Das <ramanujds9@gmail.com>
2024-09-30 12:58:37 +0530

    Updated

--- angular=-projects/hello-ust/src/app/task-card/task-card.component.ts
+++ angular=-projects/hello-ust/src/app/task-card/task-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../model/Task';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.css'
 })
