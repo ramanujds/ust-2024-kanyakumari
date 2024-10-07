8c8f9edf7a490be610ab4b66e463042fa5a5272b
Ramanuj Das <ramanujds9@gmail.com>
2024-10-07 12:22:51 +0530

    Updated with REST Api CRUD Operation

--- spring-projects/spring-webapp/src/main/java/com/ust/webapp/controller/TraineeRestController.java
+++ spring-projects/spring-webapp/src/main/java/com/ust/webapp/controller/TraineeRestController.java
@@ -3,6 +3,8 @@ package com.ust.webapp.controller;
 import com.ust.webapp.model.Trainee;
 import com.ust.webapp.service.TraineeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,32 +17,40 @@ public class TraineeRestController {
     private TraineeService traineeService;
 
     @GetMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
     public Trainee getTrainee(@PathVariable("id") int id){
         return traineeService.getTrainee(id);
     }
 
     @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
     public List<Trainee> getAllTrainees(){
         return traineeService.getAllTrainees();
     }
 
     @PostMapping
-    public Trainee saveTrainee(@RequestBody Trainee trainee){
-        return traineeService.save(trainee);
+//    @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<Trainee> saveTrainee(@RequestBody Trainee trainee){
+        Trainee savedTrainee = traineeService.save(trainee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedTrainee);
+
     }
 
 
     @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
     public Trainee updateTrainee(@PathVariable int id,@RequestBody Trainee trainee){
         return traineeService.updateTrainee(id,trainee);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTrainee(@PathVariable int id){
         traineeService.deleteTrainee(id);
     }
 
     @GetMapping("/search")
+    @ResponseStatus(code = HttpStatus.OK)
     public Trainee findTraineeByName(@RequestParam("name") String name)
     {
         return traineeService.findTraineeByName(name);
