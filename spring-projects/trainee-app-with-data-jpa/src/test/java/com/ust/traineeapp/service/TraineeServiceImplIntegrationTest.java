d1fa8f232f77e957988435b5ddcc22b4e5ee15f9
Ramanuj Das <ramanujds9@gmail.com>
2024-10-24 12:05:20 +0530

    Updated with SQL Server Configuration for AWS

--- spring-projects/trainee-app-with-data-jpa/src/test/java/com/ust/traineeapp/service/TraineeServiceImplIntegrationTest.java
+++ spring-projects/trainee-app-with-data-jpa/src/test/java/com/ust/traineeapp/service/TraineeServiceImplIntegrationTest.java
@@ -2,19 +2,20 @@ package com.ust.traineeapp.service;
 
 import com.ust.traineeapp.exception.RecordNotFoundException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TraineeServiceImplIntegrationTest {
 
     @Autowired
     TraineeServiceImpl traineeService;
 
     @Test
-    @EnabledIfEnvironmentVariable(named = "spring.profiles.active", matches = "h2")
+    @Disabled
     void getTraineeById() {
 
         // Given
