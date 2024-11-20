e8752358581917eecb798f6fe7ea2525ae2aaf33
Ramanuj Das <ramanujds9@gmail.com>
2024-11-20 09:53:18 +0530

    Added auth service with JWT impl

--- microservices-workspace/auth-service/src/main/java/com/ust/authservice/config/SecurityConfig.java
+++ microservices-workspace/auth-service/src/main/java/com/ust/authservice/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,6 +32,8 @@ public class SecurityConfig {
                                         .requestMatchers("/**").permitAll()
                                         .anyRequest().authenticated())
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                            httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
         ;
